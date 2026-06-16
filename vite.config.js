50e6d494d4f600afc3ac844e4d828c06f553319d
RazerHS <igor.kovacevic550@gmail.com>
2026-06-16 12:42:26 +0200

    updating presskit

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{
+			name: 'static-index',
+			configureServer(server) {
+				server.middlewares.use((req, _res, next) => {
+					if (req.url?.endsWith('/') && !req.url.startsWith('/@')) {
+						req.url = req.url + 'index.html';
+					}
+					next();
+				});
+			}
+		}
+	]
 });
